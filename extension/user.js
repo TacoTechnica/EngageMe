68d6e4163985af800778b1470d359fe2f9802a6e
Adris Jautakas <adrisj7@gmail.com>
2019-10-12 14:09:33 -0400

    Add user file that generates user id and add to manifest

---
+++ extension/user.js
@@ -0,0 +1,26 @@
+
+var user_id = -1;
+
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
+}
+
+chrome.storage.sync.get('userid', function(items) {
+    var userid = items.userid;
+    if (userid) {
+        useToken(userid);
+    } else {
+        userid = getRandomToken();
+        chrome.storage.sync.set({userid: userid}, function() {
+            user_id = userid;
+        });
+    }
+});
