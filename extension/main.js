d5841eafda8b2aebf4abc08719fff4cd7665c36f
Sid Pagariya <sidp@umich.edu>
2019-10-12 23:36:51 -0400

    Lots of changes!
    Signed-off-by: Murali Mohan <y2kmurali@gmail.com>

--- extension/main.js
+++ extension/main.js
@@ -1,11 +1,8 @@
-
-var video = display_intervals_get_video();
-
 if (video != null) {
   console.log("TEST: EXTENSION LOADED");
   // Load and show the engagement intervals from the backend
   display_intervals_load();
-  display_intervals_show(video);
+  display_all_intervals();
 
   // Start tracking our user's engagement
   tracker_track_engagement(video);
